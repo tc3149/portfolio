ed7f017776e32ba48694acfbd13430fbee34636e
Thomas Chen <z5313390@ad.unsw.edu.au>
2022-01-19 00:31:07 +1100

    Changes to the intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,29 +1,32 @@
-import "./intro.scss"
-import { init } from 'ityped'
-import { useEffect } from "react"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Student", "Developer", "Casual Academic"],
+    });
+  }, []);
 
-  useEffect(()=>{
-    
-  }, [])
   return (
     <div className="intro" id="intro">
-      <div className="left">
-        <div className="imgContainer">
-          <img src="assets/dunes.jpg" alt=""/>
-        </div>
-      </div>
-      <div className="right">
+      <div className="header">
         <div className="wrapper">
-          <h2>Hi There, I'm</h2>
           <h1>Thomas Chen</h1>
-          <h3>Freelance <span>Designer</span></h3>
+          <h3>
+            UNSW <span ref={textRef}></span>
+          </h3>
         </div>
+
         <a href="#portfolio">
-          <img src="assets/down.png" alt=""/>
+          <img src="assets/down.png" alt="" />
         </a>
       </div>
     </div>
-  )
+  );
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,25 @@
-import "./portfolio.scss"
+import "./portfolio.scss";
 
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://www.bankfirst.com.au/-/media/bankfirst/assets/xup-tile/191205_app2_account_v1.jpg?h=573&iar=0&w=1024&hash=DB6509A7AEDF35CE57A2A01B29BA655E"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
